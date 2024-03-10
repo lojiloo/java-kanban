77dbba73498c0af2e18660dbad7819c89d0b4ddc
Nina <gutara.ni@yandex.ru>
2024-03-10 23:37:27 +0300

    fix: исправила про отдачу важных данных, но есть вопрос по remove(0)

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -12,18 +12,22 @@ public class InMemoryHistoryManager implements HistoryManager {
     public void add(Task task) {
         if (task != null) {
             history.add(Task.copyOf(task));
-        } else {
-            return;
         }
 
         if (history.size() > 10) {
-                history.remove(0);
+            history.subList(0,1).clear();
         }
+
+        /* именно для ArrayList не нашла такого метода, который элегантно бы заменил list.remove(0). что-то
+        такое нашлось только для LinkedList... ну и есть методы в ArrayList, задающие min размер, но нигде
+        не нашла такого, чтобы ограничивал max (разве что самой прописать, но будет ли это оправданно, если
+        всегда можно просто удалить первый элемент через нулевой индекс...). В общем, мне нужен волшебный пинок в
+        нужную сторону, если это не та сторона :( */
     }
 
     @Override
     public List<Task> getHistory() {
-        return history;
+        return new ArrayList<>(history);
     }
 
 }
