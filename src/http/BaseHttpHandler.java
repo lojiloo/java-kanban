e4863b78c55daea7d9714c3528e34fe9432caf16
nina <gutara.ni@yandex.ru>
2024-07-22 01:57:43 +0300

    checkStyle: название пакета не соответствует шаблону

--- src/http/BaseHttpHandler.java
+++ src/http/BaseHttpHandler.java
@@ -1,4 +1,4 @@
-package httpAPI;
+package http;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
--- test/http/HttpTaskServerTasksTest.java
+++ test/http/HttpTaskServerTasksTest.java
@@ -1,4 +1,4 @@
-package httpAPI;
+package http;
 
 import com.google.gson.Gson;
 import managers.*;
